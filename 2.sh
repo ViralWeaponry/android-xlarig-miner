e3b9c6b4e4762b21930738b5f1cf45794f79345f
ViralWeaponry <68037525+ViralWeaponry@users.noreply.github.com>
2021-05-03 14:24:27 -0400

    Create 2.sh

---
+++ 2.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+./startubuntu.sh
+
+apt update
+
+apt-get install git build-essential cmake libuv1-dev libmicrohttpd-dev libssl-dev -y
+
+git clone https://github.com/scala-network/xlarig
+
+cd xlarig
+
+mkdir build
+
+cd build
+
+apt-get update
+
+apt-get install libhwloc-dev -y
+
+cmake ..
+
+make
